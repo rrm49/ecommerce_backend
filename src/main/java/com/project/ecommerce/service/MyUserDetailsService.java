723d38de14d76e579a2005dfe07a8489e6f36d79
Abdul-Mubeen-git <125363212+Abdul-Mubeen-git@users.noreply.github.com>
2024-12-25 17:31:07 +0530

    Adding security

---
+++ src/main/java/com/project/ecommerce/service/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.project.ecommerce.service;
+
+import com.project.ecommerce.model.UserPrincipal;
+import com.project.ecommerce.model.Users;
+import com.project.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Users user = userRepository.findByUserName(userName);
+
+        if(user == null ){
+            throw new UsernameNotFoundException("User Not Found Mawa..!");
+        }
+        return new UserPrincipal(user);
+    }
+}
